b29c6e0f5ef906a4be715a18ca8f2d1c2e2171e4
Lakshan Dias <lakshan@medium.one>
2017-07-21 07:23:03 -0700

    add python script replacement for Insight GUI when working with M1 cloud
    update cert template to match M1 signed cert
    
    note: observed signature length varies from 73 - 75

---
+++ provision.py
@@ -0,0 +1,67 @@
+import json
+
+import serial
+import serial.tools.list_ports
+import requests
+
+
+REST_WRITE_HEADERS = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+
+def send_csr(csr):
+    session = requests.session()
+
+    user_dict = {
+        "login_id": BASIC_LOGIN,
+        "password": BASIC_PASSWD,
+        "api_key": API_KEY
+    }
+    response = session.post('https://api-dev.mediumone.com/v2/login', data=json.dumps(user_dict),
+                            headers=REST_WRITE_HEADERS)
+
+    if response.status_code != 200:
+        print("Error logging in", response.content)
+        return
+
+    response = session.post("https://api-dev.mediumone.com/v2/certs", data=json.dumps({
+        "csr": csr
+    }))
+
+    if response.status_code != 200:
+        raise Exception("Error getting cert: {}".format(response.content))
+
+    body = response.json()
+    return body['crt']
+
+
+def write_to_atemel(command, data):
+    """
+    data: binary data
+    return: binary string converted from hex string response
+    """
+
+    with serial.Serial(next(x for x in serial.tools.list_ports.comports() if x.pid == 0x2404 and x.vid == 0x3eb).device, 115200) as ser:
+        ser.timeout = 5
+        ser.write('aws:{}({})\n'.format(command, ','.join(''.join('{:02X}'.format(ord(x)) for x in d) for d in data)))
+        msg = ser.read(10*1024)
+        if msg[0:2] != '00':
+            raise Exception('Atmel command failed: {}'.format(msg))
+    return ''.join(chr(int(x + y, 16)) for x, y in zip(msg[3:-1:2], msg[4:-1:2]))
+
+
+def get_csr():
+    return write_to_atemel('c', [''])
+
+
+def send_device_cert(cert):
+    write_to_atemel('sc', ['\003', cert])
+
+
+if __name__ == '__main__':
+    import sys
+
+    _, URL, API_KEY, API_BUSINESS_LOGIN, API_BUSINESS_PASSWD, BASIC_LOGIN, BASIC_PASSWD, PROJECT_MQTT_ID, BASIC_MQTT_ID  = sys.argv
+
+    csr = get_csr()
+    cert = send_csr(csr)
+    send_device_cert(cert)
